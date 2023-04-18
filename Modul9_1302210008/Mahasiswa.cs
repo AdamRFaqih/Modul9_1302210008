a5d947080392db3a505522c99c6884024f409054
adamrafiffaqih <90492899+AdamRFaqih@users.noreply.github.com>
2023-04-18 16:17:07 +0700

    Menambahkan api mahasiswa

---
+++ Modul9_1302210008/Mahasiswa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Modul9_1302210008
+{
+	public class Mahasiswa
+	{
+		public string name { get; set; }
+		public string nim { get; set; }
+		public List<string> course { get; set; }
+		public int year { get; set; }
+
+		public Mahasiswa()
+		{
+		}
+
+        public Mahasiswa(string name, string nim, List<string> course, int year)
+        {
+			this.name = name;
+			this.nim = nim;
+			this.course = course;
+			this.year = year;
+        }
+    }
+}
+
